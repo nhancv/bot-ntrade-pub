521cf1270f6e09424a0c0c4108fe8c7d8c22d5a8
Nhan Cao <nhancv92@gmail.com>
2018-08-12 17:25:20 +0700

    install websocket
    Binance wss: https://github.com/binance-exchange/binance-official-api-docs/blob/master/web-socket-streams.md
    node wss: https://www.npmjs.com/package/websocket

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,3 +28,30 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
   log('Server listening at port %d', port)
 })
+
+var WebSocketClient = require('websocket').client
+
+var client = new WebSocketClient()
+
+client.on('connectFailed', function(error) {
+  console.log('Connect Error: ' + error.toString())
+})
+
+client.on('connect', function(connection) {
+  console.log('WebSocket Client Connected')
+  connection.on('error', function(error) {
+    console.log('Connection Error: ' + error.toString())
+  })
+  connection.on('close', function() {
+    console.log('Connection Closed')
+  })
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log("Received: '" + message.utf8Data + "'")
+    }
+  })
+})
+
+client.connect(
+  'wss://stream.binance.com:9443/ws/bnbbtc@kline_5m'
+)
