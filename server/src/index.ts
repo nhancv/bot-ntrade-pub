cd4324cd77debe825e29269c7888ab5ec5da0f5d
Nhan Cao <nhancv92@gmail.com>
2018-08-15 01:25:55 +0700

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -65,6 +65,26 @@ let safeRange: number = 26 //include current
 
 client.connect(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@kline_${durTime}`)
 
+/**
+ * fetchKLineVolume
+ [
+  [
+    1499040000000,      // Open time
+    "0.01634790",       // Open
+    "0.80000000",       // High
+    "0.01575800",       // Low
+    "0.01577100",       // Close
+    "148976.11427815",  // Volume
+    1499644799999,      // Close time
+    "2434.19055334",    // Quote asset volume
+    308,                // Number of trades
+    "1756.87402397",    // Taker buy base asset volume
+    "28.46694368",      // Taker buy quote asset volume
+    "17928899.62484339" // Ignore.
+  ]
+]
+ * @param pair 
+ */
 function fetchKLineVolume(pair: string = symbol) {
   return new Promise((resolve, reject) => {
     request(
@@ -78,6 +98,18 @@ function fetchKLineVolume(pair: string = symbol) {
       }
     )
   })
+}
+
+/**
+ * Return round as floor
+ * precisionFloorRound(0.9999999999, 8)
+ * => 0.99999999
+ * @param {*} number
+ * @param {*} precision
+ */
+function precisionFloorRound(number, precision) {
+  var factor = Math.pow(10, precision)
+  return Math.floor(number * factor) / factor
 }
 
 /*************
@@ -100,15 +132,13 @@ function checking() {
           let prevVol = volArr[volArr.length - 3]
           volArr.splice(-1, 3)
           let sum = volArr.reduce((a, b) => a + b)
-          let avg = sum / volArr.length
+          let avg = precisionFloorRound(sum / volArr.length, 2)
           let maxValue = Math.max(...volArr)
           let dataLog = `Vol: ${currentVol} - Avg: ${avg} - Max: ${maxValue}`
           printLog.log(dataLog)
           if (maxValue < 4 * avg && currentVol > 7 * prevVol) {
             //trigger
-            logFile.log(dataLog 
-            + `\r\nOpen: ${currentOpen} - Close: ${currentClose} - Time: ${currentTime}`
-            )
+            logFile.log(dataLog + `\r\nOpen: ${currentOpen} - Close: ${currentClose} - Time: ${currentTime}`)
             printLog.ok('OK')
           } else {
             printLog.trace('Normal')
