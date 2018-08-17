588799503510cc9bc86c236c273a07127cf80cfc
Nhan Cao <nhancv92@gmail.com>
2018-08-17 10:06:02 +0700

    add nodemailer

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import path from 'path'
 import request from 'request'
 import printLog from 'chalk-printer'
 import logFile from 'nlogj'
+import nodemailer from 'nodemailer'
 
 logFile.setLogName('ntrade.log').clearLog()
 const app = express()
@@ -159,3 +160,44 @@ function checking() {
     }
   )
 }
+
+
+
+/**
+ * EMAIL
+ */
+// Generate test SMTP service account from ethereal.email
+// Only needed if you don't have a real mail account for testing
+nodemailer.createTestAccount((err, account) => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+          user: account.user, // generated ethereal user
+          pass: account.pass // generated ethereal password
+      }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"NTRADE 👻" <nhancv.sender@gmail.com>', // sender address
+      to: 'nhancv92@gmail.com, ', // list of receivers
+      subject: '[NTrade] Hight signal', // Subject line
+      html: '<b>Hello world?</b>' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  });
+});
